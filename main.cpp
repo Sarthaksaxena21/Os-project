22e73851822e43e6c3f222abb77c5c320e90e250
Sarthaksaxena21 <sarthaksaxena2106@gmail.com>
2026-04-23 22:37:14 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,14 @@
 #include <vector>
 #include "scheduler.h"
 #include "fcfs.h"
-
 int main() {
-
     std::vector<Process> processes = {
         Process(1, 5, 0),
         Process(2, 3, 1),
         Process(3, 8, 2),
         Process(4, 6, 3)
     };
-
     runEECSA(processes);
     runFCFS(processes);
-
     return 0;
 }
